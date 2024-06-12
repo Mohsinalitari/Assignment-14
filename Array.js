463322b896b73b7ef5843332e9f395f6d93a8d44
Mohsin Ali <166200483+Mohsinalitari@users.noreply.github.com>
2024-06-11 22:22:31 -0700

    Create Array.js

---
+++ Array.js
@@ -0,0 +1,5 @@
+//1. Write a js program to read and print elements of array.
+var StudentName=[Ali,Mohsin,Arham,Zohab,Rizwan,Rohan,Hassann];
+for(var i=0;i< Array.length;i++){
+    console.log('Print All Elemement ',StudentName[i]);
+}
